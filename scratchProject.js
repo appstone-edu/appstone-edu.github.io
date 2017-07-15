6e3fd54cc859e5f18c8dfe2712b67aaaf4ab7fcb
FlamingFireball64 <flamingfireball64@gmail.com>
2017-07-15 13:09:30 +0100

    Delete scratchProject.js

--- scratchProject.js
+++
@@ -1,3 +0,0 @@
-function scratchProject(elementId, projectId) {
-  document.getElementById(elementId).outerHTML = "<iframe id='" + elementId + "' src='https://scratch.mit.edu/projects/embed/" + projectId + "/?autostart=true' frameborder='0' ></iframe>";
-}
