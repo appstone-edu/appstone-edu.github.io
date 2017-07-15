3e228a946c75c001d10975c4de2ca86bf1a359e8
FlamingFireball64 <flamingfireball64@gmail.com>
2017-07-15 12:58:46 +0100

    Update scratchProject.js

--- scratchProject.js
+++ scratchProject.js
@@ -1,1 +1,3 @@
-
+function scratchProject(elementId, projectId) {
+  document.getElementById(elementId).outerHTML = "<iframe id='" + elementId + "' src='https://scratch.mit.edu/projects/embed/" + projectId + "/?autostart=true' frameborder='0' ></iframe>";
+}
